9672dfa53efde152c7e14c8b395834d005c8a84d
103 spkit <103spkit@gmail.com>
2022-01-29 18:30:38 +0100

    js NL

--- front/js/script.js
+++ front/js/script.js
@@ -1,37 +1,11 @@
-/*Données API localhost les products*/
-//fetch("http://localhost:3000/api/products")
-//.then(Reponse => Reponse.json())
+let url =`http://localhost:3000/api/products`;
 
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//usage:
-readTextFile("http://localhost:3000/api/products", function(text){
-    var data = JSON.parse(text);
-    
+fetch(url).then((Response) =>
+Response.json().then((data) => {
     console.log(data);
-/* dans la boucle for 
-    console.log(data[i].altTxt);
-    console.log(data[i].colors);        
-    console.log(data[i].description);
-    console.log(data[i].name);
-    console.log(data[i].imageUrl);
-    console.log(data[i].price);
-*/
-
+    document.querySelectorAll('#items').innerHTML = data.name;
     for (var i = 0; i < data.length; i++) {
-        document.getElementById("items").innerHTML += 
-        ""
+        document.getElementById("items").innerHTML += ""
 
          + data[i].altTxt + 
         + data[i].colors + 
@@ -42,33 +16,6 @@ readTextFile("http://localhost:3000/api/products", function(text){
  
         "</li></ul>";  
         }
-/*
-
-    console.log(data[i].altTxt);
-    console.log(data[i].description);
-    console.log(data[i].name);
-    console.log(data[i].imageUrl);
-    console.log(data[i].price);
-*/
-});
-
-
-// MIse en page 
-/*           <a href=./product.html?id=42">
-            <article>
-              <img src= \".../product01.jpg alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap name1</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            </article>
-          </a> 
-
- */         
-
-
-/*let p = document.createElement("p");
-
-document.getElementById("items").appendChild(p);
-
-p.innerHTML = "Mon <strong>grand</strong> contenu";
-p.classList.add("name");*/
+})
+).catch(err => console.log('erreur : ' + err));
 
