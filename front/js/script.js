959737b6431ce18bc5dd9aff1ed01587b7b24154
103 spkit <103spkit@gmail.com>
2022-01-29 19:50:30 +0100

    a_href ok

--- front/js/script.js
+++ front/js/script.js
@@ -4,18 +4,33 @@ fetch(url).then((Response) =>
 Response.json().then((data) => {
     console.log(data);
     document.querySelectorAll('#items').innerHTML = data.name;
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         document.getElementById("items").innerHTML += ""
 
-         + data[i].altTxt + 
-        + data[i].colors + 
+        let a_href = document.createElement('a');
+        document.querySelector('#items').appendChild(a_href);
+        a_href.href =  `product.html?id=${data._id}`;
+        let article = document.createElement('article');
+        a_href.appendChild(article);
+        let name = document.createElement('productName');
+        let description = document.createElement('productDescription');
+        let imageUrl = document.createElement('img');
+        let altTxt = document.createElement('alt');
+       /* + data[i].colors + 
         + data[i].name + 
         + data[i].description + 
        + "<img src=" + data[i].imageUrl + ">" +  
-        + data[i].price + 
+        + data[i].price + */
  
         "</li></ul>";  
         }
 })
 ).catch(err => console.log('erreur : ' + err));
 
+/*<a href="./product.html?id=42">
+<article>
+  <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+  <h3 class="productName">Kanap name1</h3>
+  <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+</article>
+</a> -->*/
