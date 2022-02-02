d893477881610477881905a8f21db817d37e2f0d
103SP <[103spkit@gmail.com]>
2022-02-03 00:24:45 +0100

    revoir procédure pour élément color

--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,32 @@
 const queryString_url_id = window.location.search;
 console.log(queryString_url_id);
 //const _id = queryString_url_id.slice();
-//console.log(_id); 
+//console.log(_id);
 let url = `http://localhost:3000/api/products`;
 
-let name = document.getElementById("title");
-let price = document.getElementById("price");
-let description = document.getElementById("description");
-let imageUrl = document.querySelector(".item__img");
-let img = document.createElement("img");
+let name = document.getElementById('title');
+let price = document.getElementById('price');
+let description = document.getElementById('description');
+let imageUrl = document.querySelector('.item__img');
+let img = document.createElement('img');
 imageUrl.appendChild(img);
-console.log(title);
+let colors = document.getElementById("colors")
+let colorsoption = document.createElement("option");
+console.log();
 
 ///////////////////////////////////////////
 fetch(url).then((Response) =>
   Response.json().then((data) => {
     console.log(data);
-    document.querySelectorAll('title').innerHTML = data.name;
     for (let i = 0; i < data.length; i++) {
-
-       name.innerHTML = data[i].name;
-       price.innerHTML = data[i].price;
-       description.innerHTML =data[i].description;
-       img.setAttribute("src", data[i].imageUrl);
-        img.setAttribute("alt", data[i].altTxt);
+      ////////////////////////////////////////
+      name.innerHTML = data[i].name;
+      price.innerHTML = data[i].price;
+      description.innerHTML = data[i].description;
+      img.setAttribute('src', data[i].imageUrl);
+      img.setAttribute('alt', data[i].altTxt);
+      colors.innerHTML = data[i].colors;
+      
     }
   })
 );
