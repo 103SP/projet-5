924deb00a20fcca4eb58ed6fa61b26900f3ff6e4
103SP <[103spkit@gmail.com]>
2022-02-02 22:51:56 +0100

    images en place problème window

--- front/js/product.js
+++ front/js/product.js
@@ -6,6 +6,10 @@ let url = `http://localhost:3000/api/products`;
 
 let name = document.getElementById("title");
 let price = document.getElementById("price");
+let description = document.getElementById("description");
+let imageUrl = document.querySelector(".item__img");
+let img = document.createElement("img");
+imageUrl.appendChild(img);
 console.log(title);
 
 ///////////////////////////////////////////
@@ -17,6 +21,9 @@ fetch(url).then((Response) =>
 
        name.innerHTML = data[i].name;
        price.innerHTML = data[i].price;
+       description.innerHTML =data[i].description;
+       img.setAttribute("src", data[i].imageUrl);
+        img.setAttribute("alt", data[i].altTxt);
     }
   })
 );
